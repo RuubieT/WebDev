5a468588c7af794e9c374690dec0fbaf0fc9a8ed
rubentharner <136126ruben@gmail.com>
2023-03-19 23:31:56 +0100

    Base classes for poker game
    Note that this is the base for a single player game. So the deal function must be done for every player in the game as well as the cards need to be saved somewhere.

---
+++ WebDevAPI/Db/Models/Card.cs
@@ -0,0 +1,22 @@
+﻿namespace WebDevAPI.Db.Models
+{
+    public class Card
+    {
+        public enum SUIT
+        {
+            HEARTS,
+            SPADES,
+            DIAMONDS,
+            CLUBS
+        }
+
+        public enum VALUE
+        {
+            TWO = 2, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT,
+            NINE, TEN, JACK, QUEEN, KING, ACE
+        }
+
+        public SUIT MySuit { get; set; }
+        public VALUE MyValue { get; set; }
+    }
+}
---
+++ WebDevAPI/Logic/DeckLogic/DealCards.cs
@@ -0,0 +1,25 @@
+﻿using WebDevAPI.Db.Models;
+
+namespace WebDevAPI.Logic.CardLogic
+{
+    public class DealCards
+    {
+        private Card[] playerHand;
+        private Card[] sortedHand;
+        private Card[] bordCards;
+
+        private DeckOfCards deck = new();
+
+        public DealCards()
+        {
+            playerHand= new Card[2];
+            sortedHand = new Card[5];
+            bordCards = new Card[5];
+        }
+
+        public void Deal()
+        {
+            deck.SetUpDeck();
+        }
+    }
+}
